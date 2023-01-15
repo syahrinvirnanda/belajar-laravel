48c0004afc90cbd104c93c6b2bf442e88aad9f07
syahrin virnanda <97946529+syahrinvirnanda@users.noreply.github.com>
2023-01-15 08:04:00 -0500

    belajarlaravel

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/About', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Sayhrin Virnanda",
+        "email" => "syahrinvirnanda@smk.ac.id",
+        "image" => "syahrin.jpg"
+    ]);
+});
+
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('post/{slug}', [PostController::class, 'show']);
